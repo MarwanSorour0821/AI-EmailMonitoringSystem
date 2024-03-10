245e59afe7a9a5fa248d568690e02f4197ce6f7c
MarwanSorour0821 <lw22182@bristol.ac.uk>
2024-03-10 11:58:43 +0000

    added languages

--- application.py
+++ application.py
@@ -1,30 +1,13 @@
-# from exchangelib import Account, Credentials
-
-# user_email = 'marwansorour08212003@gmail.com'
-# user_password = 'ykntzojkumfpfvtf'
-
-# credential = Credentials(user_email,user_password)
-# # account = Account(primary_smtp_address=user_email, credentials=credential, autodiscover=True)
-
-# account = Account(
-#     primary_smtp_address=user_email,
-#     credentials=credential,
-#     autodiscover=False,
-#     config={
-#         'server': 'https://outlook.office365.com/EWS/Exchange.asmx'  # Example URL, replace with your server URL
-#     }
-# )
-
-# inbox = account.inbox
-# messages = inbox.all().order_by('-datetime_received')
-
-# for email in messages:
-#     print("Subject:", email.subject)
-#     print("Content:", email.body)
-#     print("------------")
-
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 import imaplib
 import email
+
+nltk_data_path = "/Users/marwansorour/Desktop/AI-EmailMonitoringSystem"
+nltk.data.path.append(nltk_data_path)
+
+nltk.download('stopwords', download_dir=nltk_data_path)
 
 imap_server = "imap.gmail.com"
 email_address = "marwansorour08212003@gmail.com"
@@ -38,19 +21,44 @@ imap.select("Inbox")
 
 _, msgnumbers = imap.search(None, "ALL")
 
-for msgnumber in msgnumbers[0].split():
-    _, data = imap.fetch(msgnumber, "RFC822")
-    message = email.message_from_bytes(data[0][1])
+Latest_Message_Number = msgnumbers[0].split()[-1]
+_, data = imap.fetch(Latest_Message_Number, "(RFC822)")
+message = email.message_from_bytes(data[0][1])
 
-    print(f'message number; {msgnumber}')
-    print(f'From; {message.get('From')}')
-    print(f'To; {message.get('To')}')
-    print(f'BCC; {message.get('BCC')}')
-    print(f'Data; {message.get('Date')}')
+print(f'message number; {Latest_Message_Number}')
+print(f'From; {message.get('From')}')
+print(f'To; {message.get('To')}')
+print(f'BCC; {message.get('BCC')}')
+print(f'Data; {message.get('Date')}')
 
-    print("Content: ")
-    for part in message.walk():
-        if part.get_content_type() == "text/plain":
-            print(part.as_string())
+# Check for text/plain content type and print it
+part = message.get_payload()[0]  # Assuming the text/plain part is the first one
+if part.get_content_type() == "text/plain":
+    email_content = part.get_payload(decode=True).decode()  # Decode and store the content
+
+print("Content:")
+print(email_content) 
+
+nltk.download('punkt')
+#nltk.download("stopwords")
+stop_words = set(stopwords.words('english'))
+
+#tokenize email content and convert to lower case so that it is case insensitive 
+tokens = word_tokenize(email_content.lower())
+
+#Check if there are non-alphanumeric characters 
+filtered_tokens = [word for word in tokens if word.isalnum() and word not in stop_words]
+
+key_word_matching = ["urgent", "loss of opportunity", "action required", "do not want to miss", "immediately", "consequences"]
+threat_score = sum(token in key_word_matching for token in filtered_tokens)
+
+threshold = 2  # Adjust threshold as needed
+
+# Classification
+if threat_score >= threshold:
+    print("This email contains threats or urgencies.")
+else:
+    print("This email does not contain threats or urgencies.")
+
 
 imap.close()
